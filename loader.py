5d25e9a9dfd72c11333a2ff2aa72a9edd4ce3ecb
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-11-05 11:09:12 +0500

    Update Commit

--- loader.py
+++ loader.py
@@ -1,4 +1,4 @@
 from telegram.ext import ApplicationBuilder
 
-bot_token = "7308999005:AAG_zERBnhOapB_lPsU1vX8xj6YDDh5nnXg"
+bot_token = "YOUR_TOKEN"
 app = ApplicationBuilder().token(bot_token).build()
